5b0b9871271fd120b0da7394868ef2c452abab8b
nsbarr <nsbarr@gmail.com>
2014-11-19 14:57:57 -0500

    hng

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ import re
 from pprint import pprint
 import urllib2
 
-API_KEY = 'Get your own'
+API_KEY = 'ljg3wCjOnciTusPUO6b7KWNsBtShygVN'
 offset = 0
 newCollection = []
 
@@ -14,29 +14,31 @@ while offset <39000: #hardcoded whatevs
 	
 	for keyval in data["results"]["collection1"]:
 		keyword = keyval["keyword"].encode("utf-8")
-		if keyword:
+		if len(keyword) != 0:
 			#grab the first word of the entry and set it as the keyword
 			keyval["keyword"] = keyword.split(' ')[0]
 			newEntryText = ' '.join(keyword.split(' ')[1:])
-			#convert all entries to list
-			
-			# WHY?
-			if len(keyval["entry"]):
-				keyval["entry"].insert(0, newEntryText)
+			#add new entry to list of entries and
+			#convert all entries to lists (single-entry items were unicode objects)
+			if type(keyval["entry"]) == list:
+				keyval["entry"].insert(0,newEntryText)
 			else: 
 				keyval["entry"] = [newEntryText]
+		else: #remove items with empty keywords
+			data["results"]["collection1"].remove(keyval)
 
 		#remove page numbers from entries
 		newEntryList = []	
 
 		for entry in keyval["entry"]:
-			entryJoin = ' '.join(entry.split(' ')[1:])
+			entryJoin = ' '.join(entry.split(' ')[:-1])
 			newEntryList.append(entryJoin)
 		keyval["entry"] = newEntryList
 
-		newCollection.append(keyval)
+		#newCollection.append(keyval)
 
 		
 	offset += 2500
 
-pprint json.dumps(data, indent=4)
+with open('go.json', 'w') as json_data:
+	json.dump(data, json_data, indent=4)
